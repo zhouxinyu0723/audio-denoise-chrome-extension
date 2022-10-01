66b8e0fd06d41771c2e07fb71c655490e9721e3a
Xinyu Zhou <zhouxinyu0723@outlook.com>
2022-10-01 16:16:33 +0200

    v2

--- content.js
+++ content.js
@@ -1,16 +1,5 @@
 console.log("content.js starts");
 
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(sender.tab ?
-//                     "from a content script:" + sender.tab.url :
-//                     "from the extension");
-//         if (request.greeting === "hello")
-//         sendResponse({farewell: "goodbye"});
-
-
-//     });
-
 chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             console.log(sender.tab ?
@@ -19,14 +8,12 @@ chrome.runtime.onMessage.addListener(
             console.log()
             if (request.command === "streamId_messaging"){
                 console.log("get stream id: ", request.streamId);
-                
-
 
                 // chrome.tabCapture.capture({audio: true, video: false},(c) => {
                 //     console.log("captured")
                 //     console.log(c)
                 // });
-
+                // audioContext = new AudioContext();
 
                 navigator.mediaDevices.getUserMedia({
                     video: false,
@@ -39,58 +26,21 @@ chrome.runtime.onMessage.addListener(
                     }
                 })
                 .then((stream) => {
-                    console.log(stream)
-                    console.log("caputured")
+                    //console.log(stream)
+                    //manipulate_stream(stream)
+
+                    audioContext = new AudioContext();
+                    stream2audioContext = audioContext.createMediaStreamSource(stream);
+                    stream2audioContext.connect(audioContext.destination);
+                    
+                    // const video = document.querySelector('audio');
+                    // video.onloadedmetadata = (e) => {
+                    //     video.play();
+                    //   };
+
+                    //console.log("caputured")
                 });
-
-
             }
-            
             sendResponse({farewell: "goodbye"});
-
         }
     ); 
-
-// function getUserMediaHttp() {
-
-//     chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(sender.tab ?
-//                     "from a content script:" + sender.tab.url :
-//                     "from the extension");
-//         console.log()
-//         if (request.command === "streamId_messaging"){
-//             console.log("get stream id: ", request.streamId);
-            
-
-
-//             // chrome.tabCapture.capture({audio: true, video: false},(c) => {
-//             //     console.log("captured")
-//             //     console.log(c)
-//             // });
-
-//                 navigator.mediaDevices.getUserMedia({
-//                     video: false,
-//                     audio: true,
-//                     audio: {
-//                         mandatory: {
-//                             chromeMediaSource: 'tab',
-//                             chromeMediaSourceId: request.streamId
-//                         }
-//                     }
-//                 })
-//                 .then((stream) => {
-//                     console.log("caputured")
-//                 });
-
-
-
-//         }
-        
-//         sendResponse({farewell: "goodbye"});
-
-//     });
-// }
-// var script = document.createElement('script');
-// (document.head || document.body || document.documentElement).appendChild(script).text = getUserMediaHttp.toString() + ';';
-// script.remove();
